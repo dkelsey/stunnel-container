71bf8566c7fda9d63162336f30a3f7099ddef1d8
Frederik Weber <frederik.weber@bs.ch>
2018-04-25 13:18:15 +0200

    Changed log level and removed commented command

--- stunnel.sh
+++ stunnel.sh
@@ -1,9 +1,8 @@
 #!/bin/sh -e
 
 export STUNNEL_CONF="/etc/stunnel/stunnel.conf"
-export STUNNEL_DEBUG="${STUNNEL_DEBUG:-7}"
+export STUNNEL_DEBUG="${STUNNEL_DEBUG:-5}"
 export STUNNEL_CLIENT="${STUNNEL_CLIENT:-no}"
-#export STUNNEL_SNI="${STUNNEL_SNI:-}"
 export STUNNEL_CAFILE="${STUNNEL_CAFILE:-/etc/ssl/certs/ca-certificates.crt}"
 export STUNNEL_VERIFY_CHAIN="${STUNNEL_VERIFY_CHAIN:-no}"
 export STUNNEL_KEY="${STUNNEL_KEY:-/etc/stunnel/stunnel.key}"
